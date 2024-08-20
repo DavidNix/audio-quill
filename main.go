3241bceb7d0ac5d71f556af6513b30203b068a5d
David Nix <hello@davidnix.io>
2024-08-19 19:01:29 -0600

    transcribe file

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"math/rand"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -24,8 +28,8 @@ type config struct {
 func root() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "audioquill",
-		Short: "Transcribe WAV files and generate markdown files",
-		Long: `AudioQuill is a CLI tool that transcribes WAV files using Whisper (locally) and generates markdown files with titles using Ollama. 
+		Short: "Transcribe WAV files and generate text files",
+		Long: `AudioQuill is a CLI tool that transcribes WAV files using Whisper (locally) and generates text files with titles using Ollama. 
 No data leaves your machine.`,
 	}
 
@@ -34,15 +38,30 @@ No data leaves your machine.`,
 	cmd.Flags().StringVarP(&cfg.SourceDir, "source", "s", "", "source directory with WAV files")
 	_ = cmd.MarkFlagRequired("source")
 
-	cmd.Flags().StringVarP(&cfg.DestDir, "dest", "d", "", "destination directory for generated markdown files")
+	cmd.Flags().StringVarP(&cfg.DestDir, "dest", "d", "", "destination directory for generated text files")
 	_ = cmd.MarkFlagRequired("dest")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+
+		if err := os.MkdirAll(cfg.DestDir, 0777); err != nil {
+			return err
+		}
+
 		files, err := findWAVFiles(cfg.SourceDir)
 		if err != nil {
 			return fmt.Errorf("unable to find WAV source files: %w", err)
 		}
-		fmt.Println("found", files)
+		cmd.Printf("Found %d WAV files\n", len(files))
+
+		ctx := cmd.Context()
+
+		for _, wav := range files {
+			cmd.Printf("Processing %s ...\n", wav)
+			if err = processFile(ctx, cfg.DestDir, wav); err != nil {
+				return fmt.Errorf("failed to process %s: %w", wav, err)
+			}
+
+		}
 		return nil
 	}
 
@@ -61,4 +80,49 @@ func findWAVFiles(source string) ([]string, error) {
 		return nil
 	})
 	return wavFiles, err
+}
+
+func processFile(ctx context.Context, destDir string, waveFile string) error {
+	// TODO: temporary
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	randomString := make([]byte, 10)
+	for i := range randomString {
+		randomString[i] = charset[rand.Intn(len(charset))]
+	}
+
+	transcribed, err := transcribe(ctx, waveFile)
+	if err != nil {
+		return err
+	}
+
+	fpath := filepath.Join(destDir, string(randomString))
+
+	return os.WriteFile(fpath, transcribed, 0664)
+}
+
+func transcribe(ctx context.Context, waveFile string) ([]byte, error) {
+	// Call bash because whisperfile is a script that switches on the arch
+	cmd := exec.CommandContext(ctx, "bash", "-c", `./whisper-tiny.en.llamafile -f "`+waveFile+`" --no-prints`)
+	cmd.Stderr = os.Stderr
+	buf := new(bytes.Buffer)
+	cmd.Stdout = buf
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf("whisper failed %w", err)
+	}
+	b := removeTimestamps(buf.Bytes())
+	return b, nil
+}
+
+func removeTimestamps(input []byte) []byte {
+	lines := bytes.Split(input, []byte("\n"))
+	var result [][]byte
+
+	for _, line := range lines {
+		// Remove timestamp at the beginning of each line
+		if idx := bytes.Index(line, []byte("]")); idx != -1 && idx < len(line)-1 {
+			result = append(result, bytes.TrimSpace(line[idx+1:]))
+		}
+	}
+
+	return bytes.Join(result, []byte("\n"))
 }
